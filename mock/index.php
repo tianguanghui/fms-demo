21dafbd37dbb9a7c12c26885c36dad36900a2349
nimojs <nimo.jser@gmail.com>
2015-10-27 16:51:35 +0800

    美化POST数据注释

--- mock/index.php
+++ mock/index.php
@@ -3,6 +3,7 @@
     PHP 接受 FMS 的 POST 请求渲染页面
 */
 date_default_timezone_set("Shanghai/Asia");
+// __object_array 将 POST请求转换为 PHP对象
 function __object_array ($array) {
     if(is_object($array)) {  
         $array = (array)$array;  
@@ -15,13 +16,21 @@ function __object_array ($array) {
      return $array;  
 }
 $__settings = __object_array(json_decode($_POST['_fms']));
+
 /*
-    template => "/Users/nimojs/Documents/git/fms-demo/simple/view/test.php"
-    templateDir => "/Users/nimojs/Documents/git/fms-demo/simple/"
-    templatePluginDir => /Users/nimojs/Documents/git/fms-demo/simple/plugin/
-    data => {"name":1}
+    template            "/Users/nimojs/Documents/git/fms-demo/view/news.php"
+    templateDir         "/Users/nimojs/Documents/git/fms-demo/view/"
+    templatePluginDir   "/Users/nimojs/Documents/git/fms-demo/view/plugin/"
+    data                "{"title":"论数据约定在前后端配合中的重要性"}"
 */
+
+/*
+将属性绑定到全局
+随后在模板中通过 $title 获取
+*/
+
 foreach ($__settings['data'] as $key => $value) {
     $GLOBALS[$key] = $value;
 }
+// 引入模板渲染页面
 include($__settings['template']);
